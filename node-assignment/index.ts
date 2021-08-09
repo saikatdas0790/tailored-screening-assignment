9babd7bb7fd9d7fd0982eace1b50b5b8922dae91
Saikat Das <saikatdas0790@gmail.com>
2021-08-09 13:40:27 +0530

    chore: update node assignment to convert to integers and find the max

--- node-assignment/index.ts
+++ node-assignment/index.ts
@@ -2,12 +2,18 @@ const inputString = `AAggiLBJwIRIhAKgAYCiCoCAAAIIgAEICogCCJBIhEDABBCACbCQCAAAgog
 
 const decodedBufferData = Buffer.from(inputString, "base64");
 
+const unsigned32BitIntegerArray = [];
+
+for (let index = 0; index < decodedBufferData.length - 4; index++) {
+  unsigned32BitIntegerArray.push(decodedBufferData.readUInt32BE(index));
+}
+
 let largestInteger = 0;
 let foundAtIndex = -1;
 
-for (let index = 0; index < decodedBufferData.length; index++) {
-  if (largestInteger < decodedBufferData[index]) {
-    largestInteger = decodedBufferData[index];
+for (let index = 0; index < unsigned32BitIntegerArray.length; index++) {
+  if (largestInteger < unsigned32BitIntegerArray[index]) {
+    largestInteger = unsigned32BitIntegerArray[index];
     foundAtIndex = index;
   }
 }
